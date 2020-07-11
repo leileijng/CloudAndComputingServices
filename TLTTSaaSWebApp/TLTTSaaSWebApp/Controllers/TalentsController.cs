f92b2e2ee0ea0490da89d2735df1a43531675919
ZiYun1 <56954843+ZiYun1@users.noreply.github.com>
2020-07-11 15:23:13 +0800

    interfaces done

---
+++ TLTTSaaSWebApp/TLTTSaaSWebApp/Controllers/TalentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace TLTTSaaSWebApp.Controllers
+{
+    public class TalentsController : Controller
+    {
+        public ActionResult Manage()
+        {
+            ViewBag.Title = "Manage Talents";
+
+            return View();
+        }
+    }
+}
---
+++ TLTTSaaSWebApp/TLTTSaaSWebApp/Models/Talent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TLTTSaaSWebApp.Models
+{
+    public class Talent
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z-_. ]+$", ErrorMessage = "Symbols and numbers are not allowed.")]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z-_. ]+$", ErrorMessage = "Symbols and numbers are not allowed.")]
+        public string ShortName { get; set; }
+
+        [Required]
+        public string Reknown { get; set; }
+
+        [Required]
+        public string Bio { get; set; }
+    }
+}
