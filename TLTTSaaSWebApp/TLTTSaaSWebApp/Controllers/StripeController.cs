9df370e8121d5c568b09e2352e9a2823285b611e
Franky <franky.18@ichat.sp.edu.sg>
2020-07-12 16:30:20 +0800

    Disqus and Stripe

---
+++ TLTTSaaSWebApp/TLTTSaaSWebApp/Controllers/StripeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSCAssignment1.Controllers
+{
+    public class StripeController : Controller
+    {
+        // GET: Stripe
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Account()
+        {
+            return View();
+        }
+        public ActionResult Prices()
+        {
+            return View();
+        }
+        public ActionResult Comment()
+        {
+            return View();
+        }
+    }
+}
